47c4da2ebae8db6d0a1065c269a9ad8dba26096f
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2017-01-05 10:08:10 +0100

    Added the Czech language (thanks to Jan Kout)

---
+++ languages/cs/tl_page.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * cookiebar extension for Contao Open Source CMS
+ *
+ * Copyright (C) 2011-2017 Codefog
+ *
+ * @package cookiebar
+ * @author  Codefog <http://codefog.pl>
+ * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
+ * @author  Jan Kout <jan.kout@me.com>
+ * @license LGPL
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_page']['cookiebar_enable']   = [
+    'Aktivovat upozornění pro cookie',
+    'Zobrazit informační box na webové stránce.',
+];
+$GLOBALS['TL_LANG']['tl_page']['cookiebar_message']  = [
+    'Text upozornění o používání cookies',
+    'Zadejte prosím krátký popisek cookies.',
+];
+$GLOBALS['TL_LANG']['tl_page']['cookiebar_jumpTo']   = [
+    'Stránka s podrobnějšími informacemi',
+    'Zde můžete vybrat stránku, na kterou bude uživatel přesměrován pro další informace.',
+];
+$GLOBALS['TL_LANG']['tl_page']['cookiebar_position'] = [
+    'Pozice upozornění pro cookie',
+    'Zde můžete vybrat umístění upozornění pro cookie.',
+];
+$GLOBALS['TL_LANG']['tl_page']['cookiebar_button']   = [
+    'Popisek tlačítka',
+    'Zadejte prosím popisek tlačítka (z.B. <em>souhlasím</em>).',
+];
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_page']['cookiebar_legend'] = 'Cookie Information';
+
+
+/**
+ * Reference
+ */
+$GLOBALS['TL_LANG']['tl_page']['cookiebar_position']['top']             = 'nahoře';
+$GLOBALS['TL_LANG']['tl_page']['cookiebar_position']['bottom']          = 'dole';
+$GLOBALS['TL_LANG']['tl_page']['cookiebar_placement']['before_wrapper'] = 'před tagem #wrapper';
+$GLOBALS['TL_LANG']['tl_page']['cookiebar_placement']['body_end']       = 'před zakončovacím tagem body';
